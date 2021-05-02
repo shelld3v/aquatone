36bb0246fd5eaae16fc78963a6b095851bb89d18
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-02 19:47:08 +0700

    Add missing package

--- agents/url_screenshotter.go
+++ agents/url_screenshotter.go
@@ -14,6 +14,7 @@ import (
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 	"github.com/shelld3v/aquatone/core"
+	"github.com/chromedp/cdproto/emulation"
 )
 
 type URLScreenshotter struct {
