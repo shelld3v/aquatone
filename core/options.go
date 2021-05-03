a329f9a8a795760fbefea4b43cee254ff12ee0d5
shelld3v <59408894+shelld3v@users.noreply.github.com>
2021-05-03 12:04:44 +0700

    Typo

--- core/options.go
+++ core/options.go
@@ -41,7 +41,7 @@ func ParseOptions() (Options, error) {
 		TemplatePath:      flag.String("template-path", "", "Path to HTML template to use for report"),
 		Proxy:             flag.String("proxy", "", "Proxy to use for HTTP requests"),
 		MatchCodes:        flag.String("match-codes", "", "Valid HTTP status codes to do web scan (seperated by commas)"),
-		FiterCodes:        flag.String("filter-codes", "", "Invalid HTTP status codes to do web scan (seperated by commas)"),
+		FilterCodes:       flag.String("filter-codes", "", "Invalid HTTP status codes to do web scan (seperated by commas)"),
 		ChromePath:        flag.String("chrome-path", "", "Full path to the Chrome/Chromium executable to use. By default, aquatone will search for Chrome or Chromium"),
 		Ports:             flag.String("ports", "80,443,8080,8443", "Ports to scan on hosts. Supported list aliases: small, medium, large, xlarge"),
 		ScanTimeout:       flag.Int("scan-timeout", 3*1000, "Timeout in miliseconds for port scans"),
